0e83b7983618fd0d0177193e835b6913a98e0a50
Andreas Schürmann <andreas@adhook.io>
2019-08-16 09:19:01 +0200

    Added e2e test

---
+++ e2e/app.po.ts
@@ -0,0 +1,23 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTitle() {
+    return element(by.css('lsl-root h1')).getText();
+  }
+
+  getPoints() {
+    return element(by.cssContainingText('div', 'Points')).$('span').getText();
+  }
+
+  getPlus1Button() {
+    return element(by.cssContainingText('button', 'Plus 1'));
+  }
+
+  getResetButton() {
+    return element(by.cssContainingText('button', 'Reset'));
+  }
+}
