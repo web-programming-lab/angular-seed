33bcd79682cc7e833514f5dff4c98c8e5f12a0eb
Andreas Schürmann <andreas@adhook.io>
2020-02-04 11:08:46 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({ selector: 'app', 
 templateUrl: 'app.component.html', 
 styleUrls: ['./app.component.css'] })
-export class AppComponent implements OnInit {
-  registerForm: FormGroup;
+export class AppComponent {
+  model = {};
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-      this.registerForm = this.formBuilder.group({
-          firstName: ['', Validators.required],
-          email: ['', [Validators.required, Validators.email]]
-      });
-  }
+  constructor() { }
 
   onSubmit(data: any) {
       this.submitted = true;
-
-      // stop here if form is invalid
-      if (this.registerForm.invalid) {
-          return;
-      }
-
       console.log('Submit: ', data);
-  }
-
-  onReset() {
-      this.submitted = false;
-      this.registerForm.reset();
   }
 }
