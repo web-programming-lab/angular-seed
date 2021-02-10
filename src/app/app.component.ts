54b3d577c8882b00689c9df1d59368a1ed37e5ba
Andreas Schürmann <andreas@adhook.io>
2021-02-10 19:12:49 +0100

    Bugfixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 templateUrl: 'app.component.html', 
 styleUrls: ['./app.component.css'] })
 export class AppComponent {
-  model = {};
+  model: any = {};
   submitted = false;
 
   constructor() { }
