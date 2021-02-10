5e6129645f579643b3dfb396a33e6fe91e22d44b
Andreas Schürmann <andreas@adhook.io>
2021-02-10 19:28:50 +0100

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
-@Component({ selector: 'app', 
-templateUrl: 'app.component.html', 
-styleUrls: ['./app.component.css'] })
+@Component({
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css']
+})
 export class AppComponent {
+
   model: any = {};
   submitted = false;
 
   constructor() { }
 
   onSubmit(data: any) {
-      this.submitted = true;
-      console.log('Submit: ', data);
+    this.submitted = true;
+    console.log('Submit: ', data);
   }
 }
