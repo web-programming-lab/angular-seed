88cf94f195a59537d57260b8aa8439f1e2384265
Dominik <dominik.witschard@gmx.ch>
2026-02-03 08:27:18 +0100

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-import { AppModule } from './app/app.module';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
